7961d0997c382551147825a9d5bf06c0758f9e7d
haedoang <haedoang@gmail.com>
2022-08-24 17:34:04 +0900

    feat: list thymeleaf

--- spring-data-mongodb-board/src/main/java/io/haedoang/springdatamongodbboard/board/ui/BoardController.java
+++ spring-data-mongodb-board/src/main/java/io/haedoang/springdatamongodbboard/board/ui/BoardController.java
@@ -1,21 +1,14 @@
 package io.haedoang.springdatamongodbboard.board.ui;
 
 import io.haedoang.springdatamongodbboard.board.application.BoardService;
-import io.haedoang.springdatamongodbboard.board.application.dto.BoardSaveRequest;
 import io.haedoang.springdatamongodbboard.board.domain.Board;
-import io.haedoang.springdatamongodbboard.board.domain.BoardRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
 import java.util.List;
 
 /**
@@ -23,34 +16,17 @@ import java.util.List;
  * date : 2022-08-24
  * description :
  */
-@RestController
-@RequestMapping("api/v1/boards")
+@Controller
+@RequestMapping("boards")
 @RequiredArgsConstructor
 public class BoardController {
 
-    private final BoardService boardService;
-
-    @PostMapping
-    public ResponseEntity<Board> save(@RequestBody BoardSaveRequest request) {
-        Board response = boardService.save(request);
-        return ResponseEntity.created(URI.create("api/v1/boards/" + response.getId())).body(response);
-    }
+    private  final BoardService boardService;
 
     @GetMapping
-    public ResponseEntity<List<Board>> list() {
-        List<Board> response = boardService.list();
-        return ResponseEntity.ok().body(response);
-    }
-
-    @PutMapping("{id}")
-    public ResponseEntity<Void> update(@PathVariable("id") String id, @RequestBody Board board) {
-        boardService.update(id, board);
-        return ResponseEntity.ok().build();
-    }
-
-    @DeleteMapping("{id}")
-    public ResponseEntity<Void> delete(@PathVariable("id") String id) {
-        boardService.deleteById(id);
-        return ResponseEntity.noContent().build();
+    public String list(Model model) {
+        List<Board> boards = boardService.list();
+        model.addAttribute("boards", boards);
+        return "list";
     }
 }
